9e5e66b40d917091be93b5698a654282726d6139
stedn <will.stedden@gmail.com>
2020-01-15 00:23:59 -0800

    updating post image cleaner, small test tweaks to first posts

--- util/fix_blogger_images.py
+++ util/fix_blogger_images.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import glob
 import fileinput
 import sys
+import re
 
 
 files = glob.glob('*.html')
@@ -21,3 +22,22 @@ for f in files:
             if line.strip().startswith('image: '):
                 line = 'image: '+img_src + '\n'
             sys.stdout.write(line)
+
+
+
+
+
+files = glob.glob('*.html')
+for f in files:
+    with open(f) as page:
+        soup = BeautifulSoup(page, "html.parser")
+        soup.find('div', {"dir": "ltr", "trbidi":"on"}).replaceWithChildren()
+        [d.decompose() for d in soup.findAll('div', style=re.compile("display:none;"))] 
+        for div in soup.findAll('div', {"class": "separator"}):
+            img = div.a.img
+            if img:
+                img['title'] = div.text +' '+ div.a.text
+                
+                div.replaceWith(img)
+        with open("2014-12-16-daves-grill-2.html", "wb") as f_output:
+            f_output.write(soup.prettify("utf-8")) 
